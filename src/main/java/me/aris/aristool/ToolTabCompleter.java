79b0395c2863dca0b8200325a0a316e11a832145
nguyendzvennlmao <tqnmods111@gmail.com>
2026-03-29 12:17:24 +0700

    Create ToolTabCompleter.java

---
+++ src/main/java/me/aris/aristool/ToolTabCompleter.java
@@ -0,0 +1,35 @@
+package me.aris.aristool;
+
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
+import org.bukkit.entity.Player;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ToolTabCompleter implements TabCompleter {
+    @Override
+    public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
+        if (args.length == 1) {
+            return filter(Arrays.asList("give", "reload"), args[0]);
+        }
+        if (args.length == 2 && args[0].equalsIgnoreCase("give")) {
+            return filter(Bukkit.getOnlinePlayers().stream().map(Player::getName).collect(Collectors.toList()), args[1]);
+        }
+        if (args.length == 3 && args[0].equalsIgnoreCase("give")) {
+            return filter(Arrays.asList("tree_chopper", "auto_drill", "multi_tool", "drill_3x3", "drill_9x9", "firework"), args[2]);
+        }
+        if (args.length == 4 && args[0].equalsIgnoreCase("give")) {
+            return filter(Arrays.asList("60s", "30m", "1d", "7d"), args[3]);
+        }
+        return new ArrayList<>();
+    }
+
+    private List<String> filter(List<String> list, String input) {
+        return list.stream().filter(s -> s.toLowerCase().startsWith(input.toLowerCase())).collect(Collectors.toList());
+    }
+}
