5d79f386053917c5e1bc7b6589642682a7291c4a
nguyendzvennlmao <tqnmods111@gmail.com>
2026-03-29 10:46:04 +0700

    Create ArisTool.java

---
+++ src/main/java/me/aris/aristool/ArisTool.java
@@ -0,0 +1,24 @@
+package me.aris.aristool;
+
+import org.bukkit.Bukkit;
+import org.bukkit.NamespacedKey;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class ArisTool extends JavaPlugin {
+    private static ArisTool instance;
+    public static NamespacedKey EXPIRY_KEY;
+
+    @Override
+    public void onEnable() {
+        instance = this;
+        EXPIRY_KEY = new NamespacedKey(this, "expiry_time");
+        saveDefaultConfig();
+        getCommand("at").setExecutor(new ToolCommand(this));
+        getServer().getPluginManager().registerEvents(new ToolListener(this), this);
+        getServer().getGlobalRegionScheduler().runAtFixedRate(this, (task) -> {
+            Bukkit.getOnlinePlayers().forEach(player -> new ExpiryManager(this).update(player));
+        }, 20L, 20L);
+    }
+
+    public static ArisTool getInstance() { return instance; }
+                                                              }
